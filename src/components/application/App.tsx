979e40b89806e0f002fc8613596b3a6d1cdcbf6f
Oleksandr Igumnov <igumssukr@gmail.com>
2022-11-30 20:08:04 +0200

    Initial commit

---
+++ src/components/application/App.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import '../../styles/application/app.css';
+
+const App: FC = () => {
+  return (
+    <div className={"application"}>
+
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './components/application/App';
 import reportWebVitals from './reportWebVitals';
+import './styles/application/reset.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
