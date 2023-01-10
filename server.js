e0e0d0143d385a39cbc49f9f13bfe1970cf7191a
Oleksandr Igumnov <igumssukr@gmail.com>
2023-01-10 13:56:12 +0200

    Updated json server source path

--- server.js
+++ server.js
@@ -4,13 +4,8 @@ const router = jsonServer.router('./data/db.json', {});
 const middlewares = jsonServer.defaults({
     static: "./build",
 });
-const PORT = 8000;
+const PORT = process.env.PORT || 4000;
 server.use(middlewares);
-server.use(
-    jsonServer.rewriter({
-        "/api/*": "/$1",
-    })
-);
 server.use(router);
 server.listen(PORT, () => {
     console.log("Server is running");
